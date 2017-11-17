d4f0165b56aad33663a30aa96355ff3efddae422
Steffani Castro <31197673+SteffaniCastro006@users.noreply.github.com>
2017-11-16 20:39:05 -0400

    ahora si

--- Sistema/resources/views/welcome.blade.php
+++ Sistema/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Sistema de Gestión Documental UNETttttttttttt
+                    Sistema de Gestión Documental UNET
                 </div>
 
             </div>
