4a717dc793dc7c9dab2a99eb5591d4d173b4148e
Steffani Castro <31197673+SteffaniCastro006@users.noreply.github.com>
2017-11-16 20:37:42 -0400

    algo

--- Sistema/resources/views/welcome.blade.php
+++ Sistema/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Sistema de Gestión Documental UNET
+                    Sistema de Gestión Documental UNETttttttttttt
                 </div>
 
             </div>
